a79a908002e213330ade42ac22218c2fa5f33c3c
Sam <sam.takunda@gmail.com>
2024-05-03 01:51:10 +0200

    Add styles & scripts file

---
+++ script.js
@@ -0,0 +1,14 @@
+const links = [
+  "/all",
+  "/tech",
+  "/gear",
+  "/home",
+  "/cars",
+  "/tech-new",
+  "/gear-new",
+  "/home-new",
+  "/cars-new",
+  "/dashboard",
+  "/quotes",
+  "/my-account",
+];
